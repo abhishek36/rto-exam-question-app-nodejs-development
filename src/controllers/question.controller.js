56833e700c219ffaf609eb0fb2c96b4890896808
abhishek36 <akamble9913@gmail.com>
2022-01-22 14:48:42 +0530

    first commit

---
+++ src/controllers/question.controller.js
@@ -0,0 +1,30 @@
+//import Question model here
+
+const addQuestion = async (req, res) => {
+  //addQuestion api logic here
+};
+
+const getAllQuestion = async (req, res) => {
+  //getAllQuestion api logic here
+};
+
+const getQuestionById = async (req, res) => {
+  //getQuestionById api logic here
+};
+
+const updateQuestionById = async (req, res) => {
+  //updateQuestionById api logic here
+};
+
+const deleteQuestionById = async (req, res) => {
+  //deleteQuestionById api logic here
+};
+
+const QuestionController = {
+  addQuestion,
+  getAllQuestion,
+  getQuestionById,
+  updateQuestionById,
+  deleteQuestionById,
+};
+module.exports = QuestionController;
---
+++ src/controllers/symbol.controller.js
@@ -0,0 +1,30 @@
+//import Symbol model here
+
+const addSymbol = async (req, res) => {
+  //addSymbol api logic here
+};
+
+const getAllSymbol = async (req, res) => {
+  //getAllSymbol api logic here
+};
+
+const getSymbolById = async (req, res) => {
+  //getSymbolById api logic here
+};
+
+const updateSymbolById = async (req, res) => {
+  //updateSymbolById api logic here
+};
+
+const deleteSymbolById = async (req, res) => {
+  //deleteSymbolById api logic here
+};
+
+const SymbolController = {
+  addSymbol,
+  getAllSymbol,
+  getSymbolById,
+  updateSymbolById,
+  deleteSymbolById,
+};
+module.exports = SymbolController;
